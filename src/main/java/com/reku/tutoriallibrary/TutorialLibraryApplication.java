588bb96c8463daa773542cb40140d0822e2bae25
He Quanjie Boey <hqjb91@gmail.com>
2021-02-09 23:16:17 +0800

    First Commit

---
+++ src/main/java/com/reku/tutoriallibrary/TutorialLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.reku.tutoriallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reku/tutoriallibrary/TutorialLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reku.tutoriallibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
