4c7e323445b7d7ae2d2a6ee070f9d69d15beaa25
He Quanjie Boey <hqjb91@gmail.com>
2021-02-10 09:40:15 +0800

    Added Controller for Swagger generated API

--- src/main/java/com/reku/tutoriallibrary/controller/LibraryController.java
+++ src/main/java/com/reku/tutoriallibrary/controller/LibraryController.java
@@ -1,8 +1,32 @@
 package com.reku.tutoriallibrary.controller;
 
+import com.reku.tutorial_library.api.LibraryApi;
+import com.reku.tutorial_library.model.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class LibraryController {
+import java.util.ArrayList;
+import java.util.List;
 
+@RestController
+public class LibraryController implements LibraryApi {
+
+    @Override
+    public ResponseEntity<List<Book>> getAllBooksInLibrary() {
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = new Book();
+        book1.setName("Harry Potter");
+        book1.setBookAuthor("JK Rowling");
+
+        Book book2 = new Book();
+        book2.setName("Chronicles of Narnia");
+        book2.setBookAuthor("CS Lewis");
+
+        books.add(book1);
+        books.add(book2);
+
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
 }
